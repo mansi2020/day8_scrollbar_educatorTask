a34014c036a861a4a45f49bd8aff30d862572fd4
mansi2020 <sorathiyamansi11@gmail.com>
2024-01-01 22:21:06 +0530

    add functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  let [loading, setLoading] = useState(true);
+  let [scrollBarPer, setScrollBarPer] = useState(0);
+  let [message, setMessage] = useState("");
+
+  async function onComplete() {
+    setMessage("Loading completed");
+    setLoading(false);
+  }
+
+  async function changeWidthScroll() {
+    let intervalTime = setTimeout(() => {
+      setScrollBarPer((preVal) => {
+        let updateVal = preVal + 1;
+        return updateVal;
+      });
+    }, 100);
+
+    if (scrollBarPer >= 100) {
+      clearTimeout(intervalTime);
+      onComplete();
+      console.log("mansi");
+    }
+  }
+
+  useEffect(() => {
+    changeWidthScroll();
+  }, [scrollBarPer]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Progress Bar</h1>
+
+      <progress
+        value={scrollBarPer}
+        max="100"
+        className="progressBar"
+      ></progress>
+
+      {/* progrssBar text */}
+      <span className="scrollBarText">{scrollBarPer}%</span>
+
+      {loading ? <p>Loading data...</p> : <p>{message}</p>}
     </div>
   );
 }
